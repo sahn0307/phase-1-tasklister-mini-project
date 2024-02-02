b7d621e8efb427752d692fb96ebb523ae0a0adc6
Sung-Jin Ahn <sahn0307@gmail.com>
2024-02-01 20:32:37 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const form = document.querySelector('#create-task-form') 
+  const description = document.querySelector('#new-task-description')
+  const ul = document.querySelector('#tasks')
+
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const task = description.value;
+      const li = document.createElement('li');
+      li.innerText = task
+      //
+      const button = document.createElement('button')
+      button.innerText = 'x'
+      button.addEventListener('click', (e) => li.remove())
+      li.append("  ",button)
+      ul.append(li)
+      e.target.reset()
+  })
+
+})
